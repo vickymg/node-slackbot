49e4aa676c8e40c3d2fc9ae67d0b12e6ca94381f
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-12-28 16:22:55 +0000

    Set up initial files
    - create express server
    - test server returns status code of 200, and returns 'Hello, World!' at root

---
+++ test/app-test.js
@@ -0,0 +1,24 @@
+var request = require("request");
+var chai = require('chai');
+var expect = chai.expect;
+var url = 'http://localhost:3000';
+
+require("./../src/app.js");
+
+describe('Server', function() {
+
+  it("returns a status code of 200", function(done) {
+    request(url, function(err, res) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it("responds with JSON message 'Hello, World!' at the root", function(done) {
+    request(url, function(err, response, body) {
+      var payload = JSON.parse(body);
+      expect(payload.message).to.equal('Hello, World!');
+      done();
+    });
+  });
+});
